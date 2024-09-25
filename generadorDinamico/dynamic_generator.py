1991265a53fee332b34f3d53597bb5455eb7f367
IsaacF <igfu2004@gmail.com>
2024-09-25 15:09:17 -0600

    2 commit, cambio en el archivo dynamic_generator.py

--- generadorDinamico/dynamic_generator.py
+++ generadorDinamico/dynamic_generator.py
@@ -4,14 +4,14 @@ def dyn_generator(oper,state):
     """Generador dinamico.
 
     Examples:
-        >>>sum(np.array(terminar.....................
-        (Resultado)
+        >>>sum(np.array([[0,1],[1,0]]), np.array([[1,0],[0,0]]))
+        -1,0j*np.array([[0,-1],[1,0]])
 
     Args:
         oper (numpy array): First argument
         state (numpy array): Second argument
 
     Returns:
-        numpy array: Retorna la resta entre la multiplicacion de 'oper' y 'state' y la multiplicacion de 'state' y 'oper'.
+        numpy array: Retorna la resta entre la multiplicacion de 'oper' y 'state' y la multiplicacion de 'state' y 'oper' en ese orden exactamente multiplicado por -i.
     """
     return -1.0j*(np.dot(oper,state)-np.dot(state,oper))
