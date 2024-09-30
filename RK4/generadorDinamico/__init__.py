2617e0e77690e480cb50b1e01de8ca81eab9d674
IsaacF <igfu2004@gmail.com>
2024-09-30 17:33:17 -0600

    Se agregaron los archivos '__init__.py' a los modulos

---
+++ RK4/generadorDinamico/__init__.py
@@ -0,0 +1,6 @@
+"""Base para generar un sistema dinamico.
+
+Modulos exportados por este paquete:
+
+- `dyn_generator`: Provee una matriz para una sistema dinamico.
+"""
---
+++ RK4/metodoRK4/__init__.py
@@ -0,0 +1,6 @@
+"""Metodo de Runge Kutta de 4to orden.
+
+Modulos exportados por este paquete:
+
+- `metodoRK4code`: Realiza la aproximacion por medio del metodo de Runge Kutta.
+"""
